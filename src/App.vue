2ed014d37563d4bb496878150b107ff9f4581f27
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-03-28 13:14:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from './store'
+import AppTopSide from './components/AppTopSide.vue';
+
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+
+  components: {
+    AppTopSide,
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppTopSide></AppTopSide>
+    {{ store.title }}
+  </div>
+</template>
+
+<style scoped></style>
